26bd34eaf8635be5d8eaf963678a769110865ac2
Splint <79016046+Splintaz@users.noreply.github.com>
2021-07-23 17:54:26 +0200

    apachesort.py

---
+++ automate/apachesort.py
@@ -0,0 +1,15 @@
+number = 0
+number_2 = 0
+files = ["conf-available", "conf-enabled", "mods-available", "mods-enabled", "sites-available", "sites-enabled"]
+while True:
+	try:
+			with open(files[number_2]) as documents:
+				lines = documents.readlines()
+				apache2 = "/etc/apache2/"
+				path = apache2 + files[number_2] + "/" + lines[number]
+				number += 1
+				print(path)
+	except(IndexError):
+		number = 0
+		number_2 += 1
+		continue
